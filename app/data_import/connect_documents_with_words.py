867ffc8bb8d9336de1b579260746caf50441b066
VikiPeeva <peeva_viki@yahoo.com>
2017-01-13 02:33:05 +0100

    adding comments to the code

--- app/data_import/connect_documents_with_words.py
+++ app/data_import/connect_documents_with_words.py
@@ -5,36 +5,45 @@ from app.database.database import database
 
 
 def connect_documents_with_words():
-    documents = get_documents_from_database()
+    documents = get_documents_from_database()  # Get all documents from database
 
-    stop_words = get_stop_words("en")
+    stop_words = get_stop_words("en")  # Python library that we use to exclude conjunction words and other stop words
 
+    # This map we use to map every word in list of pairs: document id and position of the word in the document
     map_of_words_to_lists_with_document_position_tuples = {}
+    # This map has pairs of words and the number of documents in which the word occurs
     map_of_words_to_number_of_documents_it_occurs = {}
+    # This map is mapping every word in another map
+    # which contains document and number of occurrences of the word in that document
     map_of_words_to_map_of_document_to_number_of_occurrences_of_the_word = {}
 
-    titles = []
+    titles = []  # List of the titles of the documents
 
     total_documents = len(documents)
 
-    for document in documents:
+    for document in documents:  # For every document in the list of documents
         document_id = document.id
         title = document.title
         content = document.content
-        content_words = get_words_from_text(content)
-        titles.append(title)
+        content_words = get_words_from_text(content)  # We put all the words from the content in a list
+        titles.append(title)  # Add the title in the list of titles
 
-        set_of_words_in_document = set()
-        word_position_in_document = 0
-        for word in content_words:
-            word = word.lower()
-            word_position_in_document += 1
+        set_of_words_in_document = set()  # Set of all the words in the document content
+        word_position_in_document = 0  # Counter for the position of the words in the document
+        for word in content_words:  # For all words in the document content
+            word = word.lower()  # We convert the letters of the word to lowercase
+            word_position_in_document += 1  # Increase the position counter
 
+            # If the word is in the list of stop words or has less than three letters we exclude it as irrelevant
+            # We do not process it
             if word in stop_words or len(word) < 3:
                 continue
 
+            # If this is the first time the word is found in a document
+            # create new empty list for the document position pairs
             if word not in map_of_words_to_lists_with_document_position_tuples:
                 map_of_words_to_lists_with_document_position_tuples[word] = []
+            # Temporary list for the document position pairs of the word
             document_position_tuple_list = map_of_words_to_lists_with_document_position_tuples[word]
 
             if word not in set_of_words_in_document:
@@ -51,28 +60,36 @@ def connect_documents_with_words():
                 number_of_occurrences_of_word_in_document
             document_position_tuple_list.append((document_id, word_position_in_document))
 
+        # For every word in the document we increment for one the number of documents in which the word occurs
         for word in set_of_words_in_document:
             if word not in map_of_words_to_number_of_documents_it_occurs:
                 map_of_words_to_number_of_documents_it_occurs[word] = 0
             map_of_words_to_number_of_documents_it_occurs[word] += 1
 
+    # We clear the word list from all common words. Words that appear in more of 40% of the documents
     clear_word_lists_from_common_words(total_documents, map_of_words_to_number_of_documents_it_occurs,
                                        map_of_words_to_map_of_document_to_number_of_occurrences_of_the_word,
                                        map_of_words_to_lists_with_document_position_tuples)
 
+    # We clear the pairs document word if that word appeared less than two times in the document
     clear_word_lists_from_not_important_words(map_of_words_to_map_of_document_to_number_of_occurrences_of_the_word,
                                               map_of_words_to_lists_with_document_position_tuples, titles)
 
     print(len(map_of_words_to_lists_with_document_position_tuples))
     database.open_connection()
 
+    # For every word in the map of words to list of document position pairs
     for word in map_of_words_to_lists_with_document_position_tuples:
+        # If there are documents in which the word occurs we add it to the database
         if len(map_of_words_to_lists_with_document_position_tuples[word]) > 0:
             result = database.query("CREATE (w:Word {word: {word}}) RETURN w", {"word": word})
             id_word = 0
             for record in result:
-                id_word = record["w"].id
+                id_word = record["w"].id  # Retrieve the id to the saved word
 
+            # For every document and position of word occurrence pair
+            # we create separate relationship between the word and the document for every occurrence
+            # The created relationship has a property for the position on which the word occured
             for entry in map_of_words_to_lists_with_document_position_tuples[word]:
                 database.query("MATCH (w:Word), (doc:Document) WHERE ID(doc) = {id_doc} AND ID(w) = {id_w} "
                                "CREATE (doc)-[:CONTAINS {position: {position}}]->(w)",
@@ -111,6 +128,7 @@ def clear_word_lists_from_not_important_words(map_of_words_to_map_of_document_to
     return
 
 
+# Retrieving all documents in the database
 def get_documents_from_database():
     database.open_connection()
     result = database.query("MATCH (doc:Document) RETURN (doc)")
@@ -121,5 +139,6 @@ def get_documents_from_database():
     return documents
 
 
+# Extracting the words from a text using regular expression
 def get_words_from_text(text):
     return re.compile('[a-zA-Z_]+').findall(text)
--- app/data_import/insert_documents.py
+++ app/data_import/insert_documents.py
@@ -4,38 +4,51 @@ from app.database.database import database
 
 
 def insert_documents(document_limit, start_document_title, start_document_parent=""):
-    counter = 0
-    visited_links = set()
-    queue = Queue()
+    counter = 0  # This counter counts the added documents
+    visited_links = set()  # This set contains all visited links (documents)
+    queue = Queue()  # This queue is used for implementing the BFS algorithm
 
     database.open_connection()
 
-    start_node = {"title": start_document_title, "parent": start_document_parent}
+    start_node = {"title": start_document_title, "parent": start_document_parent}  # Creating the start node
     queue.put(start_node)
+
+    # Start BFS algorithm
     while counter < document_limit:
-        node = queue.get()
+        node = queue.get()  # Get the current node
         title = node['title']
+
+        # Check if we have visited this link previously
+        if title in visited_links:
+            continue
+
+        visited_links.add(title)  # Add the title of the document as visited
         try:
-            page = wikipedia.page(title)
+            page = wikipedia.page(title)  # Get Wikipedia page for given title
         except exceptions.DisambiguationError as e:
-            page = wikipedia.page(e.options[1])
+            page = wikipedia.page(e.options[1])  # If there are multiple pages for that title, choose the first one
+        # Add new document in the database for the given page
         database.query("CREATE (doc:Document {title: {title}, content: {content}})",
                        {"title": title, "content": page.content})
+        # If there is known page from which we got the current page, create a relationship between the two documents
         if node['parent'] != '':
             database.query(
                 "MATCH (doc1:Document {title: {title}}), (doc2:Document {title: {parent}}) "
                 "CREATE (doc1)-[:LINK]->(doc2)",
                 {"title": title, "parent": node['parent']})
 
+        # Pass all the links in the current page
         for link in page.links:
+            # If the link is not visited add it in the Queue
             if link not in visited_links:
                 queue.put({"title": link, "parent": title})
+            # Otherwise add a relationship between the current page and the visited document in the database
             else:
                 database.query(
                     "MATCH (doc1:Document {title: {title}}), (doc2:Document {title: {parent}}) "
                     "CREATE (doc1)-[:LINK]->(doc2)",
                     {"title": link, "parent": title})
 
-        counter += 1
+        counter += 1  # Increase the counter of processed documents
 
     database.close_connection()

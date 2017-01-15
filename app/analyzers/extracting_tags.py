961ecd93fbe4ef9462925acc95c5a32a6879f9c2
VikiPeeva <peeva_viki@yahoo.com>
2017-01-15 02:36:38 +0100

    extracting tags from text function added

---
+++ app/analyzers/extracting_tags.py
@@ -0,0 +1,22 @@
+from app.analyzers.similar_documents import similar_documents
+from app.database.neo4j_base_service import get_tags_for_document, get_document_for_title
+
+
+# This function returns tags for given text
+def extract_tags_from_text(text, limit=5):
+    documents = similar_documents(text, 5)  # Find the five most similar documents to the text
+    tags = set()  # Set of all tags for the text
+
+    # For every document get the tags it has and add them in the set
+    for document in documents:
+        document_tags = get_tags_for_document(document["title"])
+        for tag in document_tags:
+            tags.add(tag.value)
+        if len(tags) == limit:
+            break
+
+    return tags
+
+
+def extract_tags_from_document_content(title):
+    return extract_tags_from_text(get_document_for_title(title).content)

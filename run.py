0a2e717c527e88128b0bbca068e44147a8fd95d4
aleks <aleksandarkzuamnoski11@gmail.com>
2018-07-16 10:36:06 +0200

    add ssl certificate

--- run.py
+++ run.py
@@ -4,5 +4,7 @@ from app.test import test
 
 app = Flask(__name__, instance_relative_config=True)
 app.register_blueprint(rest)
-app.run()
+app.run(ssl_context=(
+    "./ssl/idealclient.com.crt",
+    "./ssl/idealclient.com.key"))
 app.register_blueprint(test)
